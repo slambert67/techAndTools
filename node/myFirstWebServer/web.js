7e3ad392cecfe62205feeb0d3039851111fca02a
Steve Lambert <steve.lambert@ultra-as.com>
2019-10-11 15:54:23 +0100

    wip

---
+++ node/myFirstWebServer/web.js
@@ -0,0 +1,17 @@
+var http = require("http"); // allows program to act as web server
+
+function process_request(req, res) {
+
+  var body = 'Thanks for calling\n';
+  var content_length = body.length;
+  
+  // construct response
+  res.writeHead( 200, {
+    'Content-Length': content_length,
+    'Content-Type': 'text/plain'
+  });
+  res.end(body);
+}
+
+var s = http.createServer(process_request);
+s.listen(8080);
