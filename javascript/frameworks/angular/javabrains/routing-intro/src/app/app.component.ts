e5d19a9b0922fa3e226963c2dcffe9b4a9508134
Steve Lambert <steve.lambert@ultra-as.com>
2022-08-15 11:16:55 +0100

    wip

---
+++ javascript/frameworks/angular/javabrains/routing-intro/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'routing-intro';
+
+  routes = [
+    {linkName: 'Home', url: 'home'},
+    {linkName: 'Settings', url: 'settings'}
+  ]
+}
