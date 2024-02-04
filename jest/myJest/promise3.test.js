48d86c8bfd10f5306204d2f2d5d75cdd6c74b231
Steve Lambert <steve.lambert@adbsafegate.com>
2024-02-04 15:38:27 +0000

    wip

--- jest/myJest/promise3.test.js
+++ jest/myJest/promise3.test.js
@@ -1,12 +1,22 @@
 let sandbox = require('./promise3');
 
 beforeEach( () => {
+    // jest.useFakeTimers({ doNotFake: ['nextTick'] });
     jest.useFakeTimers();
 });
 
 afterAll(() => {
     jest.useRealTimers()
 })
+
+test("1", async () => {
+
+    const myPromise = sandbox.returnPromise('squoink');
+    jest.advanceTimersByTime(2000);
+    // jest.runOnlyPendingTimers();
+    // await new Promise(process.nextTick);
+    return expect(myPromise).resolves.toEqual({msg: 'It works', data: 'squoink'});
+});
 
 test("2", async () => {
 
